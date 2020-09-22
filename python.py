448d3fd4546dff10b7b0435891129765405bfdc8
elee <ericlee.2311@gmail.com>
2020-09-22 10:13:12 -0400

    range function

--- python.py
+++ python.py
@@ -1,11 +1,22 @@
-print('Hello World!')
+# print('Hello World!')
 
-list = [1, 2, 3, 4]
-print(list)
-print(len(list))
+# list = [1, 2, 3, 4]
+# print(list)
+# print(len(list))
 
 #Fibonnaci Sequence
 a , b = 0, 1
 while a < 10:
-    print(a)
+    print(a, end = ',')
     a, b = b, a+b
+
+#For statements
+list = ['the', 'cat', 'in', 'the', 'hat']
+for w in list:
+    print(w, len(w))
+
+#Range function
+n = 100
+for i in range(n):
+    if i % 2 == 0:
+        print(i)
