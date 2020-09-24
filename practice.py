f630413dce851650f5d5616f14f75802ae842c83
elee <ericlee.2311@gmail.com>
2020-09-24 12:06:37 -0400

    dig_pow

--- practice.py
+++ practice.py
@@ -224,3 +224,47 @@ def domain_name(url):
                 m.remove(x)
             
     return m[0]
+
+# Some numbers have funny properties. For example:
+
+# 89 --> 8¹ + 9² = 89 * 1
+
+# 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+# 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+# Given a positive integer n written as abcd... (a, b, c, d... being digits) 
+# and a positive integer p
+
+# we want to find a positive integer k, if it exists, such as the sum of the 
+# digits of n taken to the successive powers of p is equal to k * n.
+# In other words:
+
+# Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+# If it is the case we will return k, if not return -1.
+
+# Note: n and p will always be given as strictly positive integers
+
+def dig_pow(n, p):
+    str_num = list(str(n))
+    
+    for i in range(len(str_num)):
+        str_num[i] = int(str_num[i])
+    
+    prod = 0
+    i = 0
+    
+    while i < len(str_num):
+        prod += str_num[i]**p
+        p += 1
+        i += 1
+        
+    result = str(prod/n).split('.')
+    
+    if result[0] == '0':
+        return -1
+    elif int(result[1]) > 0:
+        return -1
+    else:
+        return int(result[0])
