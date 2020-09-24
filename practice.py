d0edf6b05c4dc53c78f2cc71c3be248c3d74adde
elee <ericlee.2311@gmail.com>
2020-09-24 16:47:32 -0400

    sort_array

--- practice.py
+++ practice.py
@@ -294,3 +294,26 @@ def get_middle(s):
         return s[len(s)//2]
     else:
         return s[(len(s)//2)-1] + s[len(s)//2]
+
+# You have an array of numbers.
+# Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+# Zero isn't an odd number and you don't need to move it. If you have an 
+# empty array, you need to return it.
+
+# Example
+
+# sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+
+def sort_array(source_array):
+    odds = list(filter(lambda x: x % 2 == 1, source_array))
+    odds.sort()
+    
+    j = 0
+    
+    for i in range(len(source_array)):
+        if source_array[i] % 2 == 1:
+            source_array[i] = odds[j]
+            j += 1
+            
+    return source_array
