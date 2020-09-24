e632502f6bffbd6717aa121bdb7f46760c130148
elee <ericlee.2311@gmail.com>
2020-09-24 13:36:36 -0400

    get_middle

--- practice.py
+++ practice.py
@@ -268,3 +268,29 @@ def dig_pow(n, p):
         return -1
     else:
         return int(result[0])
+
+# You are going to be given a word. Your job is to return the middle 
+# character of the word. If the word's length is odd, return the middle character. 
+# If the word's length is even, return the middle 2 characters.
+
+# #Examples:
+
+# Kata.getMiddle("test") should return "es"
+
+# Kata.getMiddle("testing") should return "t"
+
+# Kata.getMiddle("middle") should return "dd"
+
+# Kata.getMiddle("A") should return "A"
+
+def get_middle(s):
+    
+    if len(s) == 1:
+        return s
+    elif len(s) == 2:
+        return s[:2]
+    
+    if len(s) % 2 != 0:
+        return s[len(s)//2]
+    else:
+        return s[(len(s)//2)-1] + s[len(s)//2]
