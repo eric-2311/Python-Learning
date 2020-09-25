b284adcf7233a10dd6651ab51ba9d3499a580875
elee <ericlee.2311@gmail.com>
2020-09-25 18:23:46 -0400

    optimized num_teams! yes!

--- practice.py
+++ practice.py
@@ -394,3 +394,31 @@ def numTeams(rating):
             result.append(x)
             
     return len(result)
+
+def num_teams(rating):
+    '''More optimized solution for num_teams'''
+    import pdb
+    # pdb.set_trace()
+    teams = 0
+    l = []
+    x = 0
+
+    while x <= len(rating)-2:
+        y = x + 1
+        while y <= len(rating)-1:
+            z = y + 1
+            while z < len(rating):
+                if rating[x] < rating[y] and rating[y] < rating[z]:
+                    teams += 1
+                    l.append([rating[x], rating[y], rating[z]])
+                elif rating[x] > rating[y] and rating[y] > rating[z]:
+                    teams += 1
+                    l.append([rating[x], rating[y], rating[z]])
+
+                z += 1
+            y += 1
+        x += 1
+
+    
+    print(l)
+    print(teams)
--- you_buggin.py
+++ you_buggin.py
@@ -1,22 +1,28 @@
 def num_teams(rating):
     import pdb
-    pdb.set_trace()
+    # pdb.set_trace()
     teams = 0
-    
+    l = []
     x = 0
 
-    while x < len(rating)-2:
-        y = 0
-
-        while y < len(rating)-1:
-            y += 1
+    while x <= len(rating)-2:
+        y = x + 1
+        while y <= len(rating)-1:
             z = y + 1
             while z < len(rating):
                 if rating[x] < rating[y] and rating[y] < rating[z]:
                     teams += 1
+                    l.append([rating[x], rating[y], rating[z]])
+                elif rating[x] > rating[y] and rating[y] > rating[z]:
+                    teams += 1
+                    l.append([rating[x], rating[y], rating[z]])
 
                 z += 1
+            y += 1
+        x += 1
 
+    
+    print(l)
     print(teams)
 
 
