55c69e5de495b93ddee9b38635c7050d6c448471
elee <ericlee.2311@gmail.com>
2020-10-01 18:41:03 -0400

    refactor num_teams

--- data_structures_and_algos.py
+++ data_structures_and_algos.py
@@ -281,7 +281,6 @@ def num_teams(rating):
     import pdb
     # pdb.set_trace()
     teams = 0
-    l = []
     x = 0
 
     while x <= len(rating)-2:
@@ -291,10 +290,8 @@ def num_teams(rating):
             while z < len(rating):
                 if rating[x] < rating[y] and rating[y] < rating[z]:
                     teams += 1
-                    l.append([rating[x], rating[y], rating[z]])
                 elif rating[x] > rating[y] and rating[y] > rating[z]:
                     teams += 1
-                    l.append([rating[x], rating[y], rating[z]])
 
                 z += 1
             y += 1
